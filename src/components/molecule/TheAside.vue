ea592ea793be7338281f6d7ed134dab291cd7870
jean.michel <mikeribeirooficial@gmail.com>
2024-02-29 14:44:42 -0300

    feat: init sidebar content

--- src/components/molecule/TheAside.vue
+++ src/components/molecule/TheAside.vue
@@ -3,5 +3,47 @@
 </script>
 
 <template>
-  <h1> aside</h1>
+  <div class="aside">
+    <h2>Minhas Sprints</h2>
+    <hr>
+
+    <div class="sprints__list">
+      <ul>
+        <li>Sprint #01</li>
+        <li>Sprint #02</li>
+        <li>Sprint #03</li>
+        <li>Sprint #04</li>
+        <li>Sprint #05</li>
+        <li>Sprint #06</li>
+        <li>Sprint #01</li>
+        <li>Sprint #02</li>
+        <li>Sprint #03</li>
+        <li>Sprint #04</li>
+        <li>Sprint #05</li>
+        <li>Sprint #06</li>
+      </ul>
+    </div>
+
+  </div>
 </template>
+
+<style scoped lang="scss">
+.aside {
+  .sprints__list {
+    padding: 1.5rem;
+
+    li {
+      margin-bottom: 16px;
+      cursor: pointer;
+      padding: 4px 8px;
+      border-radius: 8px;
+      background-color: var(--grey-200);
+    }
+  }
+
+  hr {
+    margin: 16px 0;
+  }
+}
+
+</style>
